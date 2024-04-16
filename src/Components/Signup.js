ae172bf5b6274a4a6e5542739c4a6ea80babedb1
Swapna Sarit Mallick <imswapnasarit@gmail.com>
2024-04-16 14:18:05 +0530

    made sign up page

--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -1,17 +1,41 @@
 import React from "react";
 import { useState } from "react";
 import "../Components/Signup.css";
+
+
 function Signup() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [isSigningUp, setIsSigningUp] = useState(false); // State variable to track whether user wants to sign up
+
+  const toggleSignup = () => {
+    setIsSigningUp(!isSigningUp);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (isSigningUp) {
+      // Handle account creation logic here
+      console.log(
+        "Creating account with email:",
+        email,
+        "and password:",
+        password
+      );
+    } else {
+      // Handle login logic here
+      console.log("Logging in with email:", email, "and password:", password);
+    }
+  };
+
   return (
     <div className="signup-container">
-      <form>
-        <h2>Sign Up</h2>
+      <form onSubmit={handleSubmit}>
+        <h2>{isSigningUp ? "Create Account" : "Login"}</h2>
         <input
           className="input-field"
           type="text"
-          placeholder="email"
+          placeholder="Email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
@@ -19,12 +43,19 @@ function Signup() {
         <input
           className="input-field"
           type="password"
-          placeholder="password"
+          placeholder="Password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
-        <button className="signup-btn">Sign Up</button>
+        <br />
+        <button type="submit" className="signup-btn">
+          {isSigningUp ? "Create Account" : "Login"}
+        </button>
       </form>
+
+      <button className="toggle-btn" onClick={toggleSignup}>
+        {isSigningUp ? "Already have an account? Login" : "Create an Account"}
+      </button>
     </div>
   );
 }
