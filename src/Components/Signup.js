94dbb555d2e4141fa3c0868f77db0afb282e5e82
Swapna Sarit Mallick <imswapnasarit@gmail.com>
2024-04-16 11:53:20 +0530

    first commit

---
+++ src/Components/Signup.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { useState } from "react";
+import "../Components/Signup.css";
+function Signup() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  return (
+    <div className="signup-container">
+      <h2>Sign Up</h2>
+      <input
+        className="input-field"
+        type="text"
+        placeholder="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <br />
+      <input
+        className="input-field"
+        type="password"
+        placeholder="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button className="signup-btn">Sign Up</button>
+    </div>
+  );
+}
+
+export default Signup;
